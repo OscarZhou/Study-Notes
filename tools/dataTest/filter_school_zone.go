a82703f266f5b3662a4d439a488244a96dda6b48
OscarZhou <zhouhongyu1989@gmail.com>
2018-07-03 11:22:54 +1200

    Fix the bug: adjust data format

--- tools/dataTest/filter_school_zone.go
+++ tools/dataTest/filter_school_zone.go
@@ -23,8 +23,21 @@ func FilterSchoolZone() error {
 	// 	models.SchoolZone{},
 	// )
 
+	err = groupGeoPosition(db)
+	if err != nil {
+		return err
+	}
+
+	// err = removeAllComma(db)
+	// if err != nil {
+	// 	return err
+	// }
+	return nil
+}
+
+func groupGeoPosition(db *gorm.DB) error {
 	var schoolZones []models.SchoolZone
-	err = db.Preload("Surfaces").Find(&schoolZones).Error
+	err := db.Preload("Surfaces").Find(&schoolZones).Error
 	if err != nil {
 		return err
 	}
@@ -41,16 +54,38 @@ func FilterSchoolZone() error {
 				}
 			}
 			v.Surfaces[si].PosList = strings.Join(newList, ",")
-			fmt.Println(v.Surfaces[si])
 			err = tx.Table("school_zone_surfaces").Where("school_zone_id = ?", v.Surfaces[si].SchoolZoneID).Updates(map[string]interface{}{"pos_list": v.Surfaces[si].PosList}).Error
 			if err != nil {
 				tx.Rollback()
 				return err
 			}
+			fmt.Println("SchoolZoneID = ", surface.SchoolZoneID)
 		}
 	}
 
 	tx.Commit()
 	return nil
+}
 
+func removeAllComma(db *gorm.DB) error {
+	var schoolZones []models.SchoolZone
+	err := db.Preload("Surfaces").Find(&schoolZones).Error
+	if err != nil {
+		return err
+	}
+
+	tx := db.Begin()
+	for _, v := range schoolZones {
+		for si, surface := range v.Surfaces {
+			v.Surfaces[si].PosList = strings.Replace(surface.PosList, ",", " ", -1)
+			err = tx.Table("school_zone_surfaces").Where("school_zone_id = ?", v.Surfaces[si].SchoolZoneID).Updates(map[string]interface{}{"pos_list": v.Surfaces[si].PosList}).Error
+			if err != nil {
+				tx.Rollback()
+				return err
+			}
+			fmt.Println("SchoolZoneID = ", surface.SchoolZoneID)
+		}
+	}
+	tx.Commit()
+	return nil
 }
